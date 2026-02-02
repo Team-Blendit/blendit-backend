ed7ec216c2a18e1a84ae93bd08dab5a13e3485f8
Ting97ok <89396424+Ting97ok@users.noreply.github.com>
2026-02-02 23:13:06 +0900

    [Fix] 유저 프로필 이미지 추가

--- blendit-api/src/main/java/kr/blendit/api/user/controller/UserProfileController.java
+++ blendit-api/src/main/java/kr/blendit/api/user/controller/UserProfileController.java
@@ -9,12 +9,16 @@ import kr.blendit.api.user.controller.dto.UpdateUserProfileRequest;
 import kr.blendit.api.user.facade.UserProfileFacade;
 import kr.blendit.common.security.jwt.CurrentUser;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "유저 프로필 API")
 @RestController
@@ -25,9 +29,11 @@ public class UserProfileController {
   private final UserProfileFacade userProfileFacade;
 
   @Operation(summary = "유저 프로필 수정")
-  @PutMapping
-  public void updateUserProfile(CurrentUser currentUser, @RequestBody @Valid UpdateUserProfileRequest request) {
-    userProfileFacade.updateUserProfile(currentUser, request);
+  @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public void updateUserProfile(CurrentUser currentUser,
+      @ModelAttribute @ParameterObject @Valid UpdateUserProfileRequest request,
+      @RequestPart(value = "profileImage", required = false) MultipartFile profileImage) {
+    userProfileFacade.updateUserProfile(currentUser, request, profileImage);
   }
 
   @Operation(summary = "유저 프로필 조회")
--- blendit-api/src/main/java/kr/blendit/api/user/facade/UserProfileFacade.java
+++ blendit-api/src/main/java/kr/blendit/api/user/facade/UserProfileFacade.java
@@ -6,13 +6,16 @@ import kr.blendit.api.keyword.service.KeywordService;
 import kr.blendit.api.user.controller.dto.GetMyProfileResponse;
 import kr.blendit.api.user.controller.dto.GetUserProfileResponse;
 import kr.blendit.api.user.controller.dto.UpdateUserProfileRequest;
+import kr.blendit.api.user.domain.User;
 import kr.blendit.api.user.service.UserProfileService;
+import kr.blendit.api.user.service.UserService;
 import kr.blendit.common.security.jwt.CurrentUser;
 import kr.blendit.common.storage.dto.UploadedObject;
 import kr.blendit.common.storage.service.LocalObjectStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +24,13 @@ public class UserProfileFacade {
   private final UserProfileService userProfileService;
   private final KeywordService keywordService;
   private final LocalObjectStorageService localObjectStorageService;
+  private final UserService userService;
 
   @Transactional
-  public void updateUserProfile(CurrentUser currentUser, UpdateUserProfileRequest request) {
-    UploadedObject uploadedObject = localObjectStorageService.upload(request.profileImage(), "user-profile-image");
+  public void updateUserProfile(CurrentUser currentUser, UpdateUserProfileRequest request, MultipartFile profileImage) {
+    User user = userService.getUser(currentUser.getUserUuid());
+    localObjectStorageService.deleteByUrl(user.getProfileImage());
+    UploadedObject uploadedObject = localObjectStorageService.upload(profileImage, "user-profile-image");
     List<Keyword> keywordList = keywordService.getKeywordList(request.keywordUuidList());
     userProfileService.updateUserProfile(
         currentUser,
