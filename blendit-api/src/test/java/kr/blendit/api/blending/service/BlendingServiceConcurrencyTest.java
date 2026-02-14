85ddbccd71d3f246730c46b4ad4aab354783da57
infreeJ <kasious97@naver.com>
2026-02-14 23:32:57 +0900

    test DB 사용

--- blendit-api/src/test/java/kr/blendit/api/blending/service/BlendingServiceConcurrencyTest.java
+++ blendit-api/src/test/java/kr/blendit/api/blending/service/BlendingServiceConcurrencyTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import java.lang.reflect.Constructor;
@@ -29,6 +30,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlendingServiceConcurrencyTest {
 
   @Autowired
